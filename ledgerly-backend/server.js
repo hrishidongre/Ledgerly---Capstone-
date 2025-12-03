9c02cd96ca424e9e26c57a97693db0ea1f7108be
hrishidongre <hrishi.dongre2024@nst.rishihood.edu.in>
2025-12-03 14:25:05 +0530

    Fixed CORS atttempt: 2

--- ledgerly-backend/server.js
+++ ledgerly-backend/server.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://budget-ease-capstone.vercel.app/"
+    "https://budget-ease-capstone.vercel.app"
   ],
   credentials: true,
 }))
